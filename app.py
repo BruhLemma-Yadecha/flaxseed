69015af08a72e17f9c4387bd3bd1177a49e84129
SujanMaddipati <137219736+SujanMaddipati@users.noreply.github.com>
2024-01-27 18:53:37 -0500

    Working changes

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ import joblib
 import pandas as pd
 import numpy as np
 
-model = joblib.load("/static/gnb_model.joblib")
+model = joblib.load("static/gnb_model.joblib")
+print(model)
 app = Flask(__name__)
 
 @app.route("/")
@@ -12,11 +13,11 @@ def home():
     return render_template("flaxseed.html")
 
 def predict(df_test):
-    df_test = pd.get_dummies(df_test, columns = ["Payment_currency"], type=int) 
-    df_test = pd.get_dummies(df_test, columns = ["Received_currency"], type=int) 
-    df_test = pd.get_dummies(df_test, columns = ["Sender_bank_location"], type=int) 
-    df_test = pd.get_dummies(df_test, columns = ["Receiver_bank_location"], type=int) 
-    df_test = pd.get_dummies(df_test, columns = ["Payment_type"], type=int) 
+    df_test = pd.get_dummies(df_test, columns = ["Payment_currency"], dtype=int) 
+    df_test = pd.get_dummies(df_test, columns = ["Received_currency"], dtype=int) 
+    df_test = pd.get_dummies(df_test, columns = ["Sender_bank_location"], dtype=int) 
+    df_test = pd.get_dummies(df_test, columns = ["Receiver_bank_location"], dtype=int) 
+    df_test = pd.get_dummies(df_test, columns = ["Payment_type"], dtype=int) 
     
     prediction = model.predict(df_test)
     return prediction
