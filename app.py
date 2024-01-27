848363c0c856fa7b40a9a8fd2060631c3b70a3cc
SujanMaddipati <137219736+SujanMaddipati@users.noreply.github.com>
2024-01-27 18:35:53 -0500

    Updates

--- app.py
+++ app.py
@@ -7,12 +7,19 @@ def home():
 
     return render_template("flaxseed.html")
 
-def predict(test){
+def predict(df_test):
     import joblib
-    model = joblib.load()
+    import pandas as pd
+    import numpy as np
+    df_test = pd.get_dummies(df_test, columns = ["Payment_currency"], dtype=int) 
+    df_test = pd.get_dummies(df_test, columns = ["Received_currency"], dtype=int) 
+    df_test = pd.get_dummies(df_test, columns = ["Sender_bank_location"], dtype=int) 
+    df_test = pd.get_dummies(df_test, columns = ["Receiver_bank_location"], dtype=int) 
+    df_test = pd.get_dummies(df_test, columns = ["Payment_type"], dtype=int) 
+    model = joblib.load("/static/gnb_model.joblib")
 
+    
 
-}
     
 
 
