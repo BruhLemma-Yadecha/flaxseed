d4bd7526bf513dbbb5bbb19ca84c85c891269dc1
SujanMaddipati <137219736+SujanMaddipati@users.noreply.github.com>
2024-01-27 18:45:48 -0500

    Changes updtes, joblib not working

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template
+import joblib
+import pandas as pd
+import numpy as np
 
+model = joblib.load("/static/gnb_model.joblib")
 app = Flask(__name__)
 
 @app.route("/")
@@ -8,15 +12,15 @@ def home():
     return render_template("flaxseed.html")
 
 def predict(df_test):
-    import joblib
-    import pandas as pd
-    import numpy as np
-    df_test = pd.get_dummies(df_test, columns = ["Payment_currency"], dtype=int) 
-    df_test = pd.get_dummies(df_test, columns = ["Received_currency"], dtype=int) 
-    df_test = pd.get_dummies(df_test, columns = ["Sender_bank_location"], dtype=int) 
-    df_test = pd.get_dummies(df_test, columns = ["Receiver_bank_location"], dtype=int) 
-    df_test = pd.get_dummies(df_test, columns = ["Payment_type"], dtype=int) 
-    model = joblib.load("/static/gnb_model.joblib")
+    df_test = pd.get_dummies(df_test, columns = ["Payment_currency"], type=int) 
+    df_test = pd.get_dummies(df_test, columns = ["Received_currency"], type=int) 
+    df_test = pd.get_dummies(df_test, columns = ["Sender_bank_location"], type=int) 
+    df_test = pd.get_dummies(df_test, columns = ["Receiver_bank_location"], type=int) 
+    df_test = pd.get_dummies(df_test, columns = ["Payment_type"], type=int) 
+    
+    prediction = model.predict(df_test)
+    return prediction
+
 
     
 
