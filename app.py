50b9074a39943814a5c51d58cdf802183db28dd1
SujanMaddipati <137219736+SujanMaddipati@users.noreply.github.com>
2024-01-27 19:36:24 -0500

    Changes

--- app.py
+++ app.py
@@ -1,16 +1,38 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session, flash, url_for, redirect
 import joblib
 import pandas as pd
 import numpy as np
 
 model = joblib.load("static/gnb_model.joblib")
+print(model)
 app = Flask(__name__)
-app.config["SECRET_KEY"] = 'cypher.ai flaxseed'
 
-@app.route("/")
+@app.route("/", methods =["GET", "POST"])
 def home():
+    if request.method == "POST":
+        s_account = request.form.get("s_account")
+        r_account = request.form.get("r_account")
+        amount = request.form.get("amount")
+        p_currency = request.form.get("p_currency")
+        r_currency = request.form.get("r_currency")
+        sb_location = request.form.get("sb_location")
+        rb_location = request.form.get("rb_location")
+        p_type = request.form.get("p_type")
+        values = [s_account, r_account, amount, p_currency, r_currency, sb_location, rb_location, p_type]
+        df = pd.DataFrame({"Test": values})
+        prediction = predict(df)
+        session['prediction'] = 'prediction'
+        redirect(output)
+    else:
+        return render_template("flaxseed.html")
 
-    return render_template("flaxseed.html")
+@app.route("/output", methods =["GET", "POST"])
+def output():
+    prediction = session.get('prediction', None)
+    # flash('prediction')
+    return render_template("flaxseed_2.html", prediction = prediction)
+    
+
 
 def predict(df_test):
     df_test = pd.get_dummies(df_test, columns = ["Payment_currency"], dtype=int) 
